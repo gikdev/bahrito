03bb499022e288176be27bbb57d56ac12927d218
Mohammad Mahdi Bahrami <m.m.bhrm85@gmail.com>
2024-11-19 23:33:56 +0330

    Added a day count feature but it's commented...

--- src/shared/helpers.ts
+++ src/shared/helpers.ts
@@ -1,5 +1,7 @@
 function minutesToClock(minutes: number): string {
   const addZero = (num: number) => num.toString().padStart(2, "0")
+  // const d = Math.floor(minutes / 60 / 24)
+  // const h = addZero(Math.floor(minutes / 60 - d * 24))
   const h = addZero(Math.floor(minutes / 60))
   const m = addZero(minutes % 60)
 
